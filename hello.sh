7a7a8231a98ed9607c3852ee8eb330eb6e387163
LidiyaReju <146076033+LidiyaReju@users.noreply.github.com>
2024-02-26 11:06:57 +0530

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,17 @@
+echo "Enter your Details"
+echo "Enter your name"
+read name
+echo "Enter your semester"
+read sem
+echo "Enter your batch"
+read batch
+echo "Enter your rollno"
+read rollno
+echo "Enter your register number"
+read regno
+echo "--------------------"
+echo "name:$name"
+echo "semester:$sem"
+echo "Batch:$batch"
+echo "Rollno:$rollno"
+echo "Regno:$regno"
